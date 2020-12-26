eb6ce3eff8188ddcaf38d9bce1ce8228f75d6c03
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-26 18:52:26 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 import pathlib
 
 import yaml
+from sqlalchemy_imageattach.stores.fs import FileSystemStore
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
 config_path = BASE_DIR / 'address_book_aiohttp' / 'config.yaml'
+DSN = 'postgresql://{user}:{password}@{host}:{port}/{database}'
 
 
 def get_config(path):
@@ -13,3 +15,8 @@ def get_config(path):
 
 
 config = get_config(config_path)
+
+store = FileSystemStore(
+    path='media/images',
+    base_url='http://127.0.0.1:8000/'
+)
